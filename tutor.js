88bb342716c8ed20fd3c90a90c24ae11329f5df1
Maksim <riksancho1@gmail.com>
2024-09-17 20:22:03 +0200

    add first obj

---
+++ tutor.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+const leftBorderWidth =1;
+
+const number = 10;
+console.log(number);
+
+const obj ={
+    a: 50
+};
+obj.a =10;
+console.log(obj);
+{
+    let result = 50;
+    console.log(result);
+}
+const userName = "John";
+let NUMber = 25;
+NUMber = 24;
+console.log(number);
+
+const supermarket = {
+    orange: 1,
+    apple: 6,
+    juice: 9
+};
+console.log(supermarket.juice);
+
+let hotel = ["appart1 ","appart2 ", "appart3 "];
+let freeHotel = [true, false, true];
+let floorHotel = ["1floor ", "2floor ", "2floor "];
+console.log(floorHotel[0]+hotel[0]+freeHotel[0]);
+
+const figureTime = {
+    Time: 15,
+    Door: "wood" 
+};
+
+const category = 'yellowList';
+console.log(`https://googleurl.com/${category}/4`);
+

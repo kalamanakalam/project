9f1574b3258d59bfabc939c8330dd9cc9aa3a50d
Maksim <riksancho1@gmail.com>
2024-09-17 20:50:02 +0200

    update second

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели ?","1");
 const personalMovieDB = {
-    count:0,
-    movies:0,
-    actors:0,
-    genres:0,
-    privat:0,
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
 };

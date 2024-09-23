805ebd0b5896db7cfc7b2425bd9a9cf888c70209
Maksim <riksancho1@gmail.com>
2024-09-23 19:39:47 +0200

    wrapped all code to function

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function start(){
     }
 }
 
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -17,33 +19,42 @@ const personalMovieDB = {
     privat:false,
 };
 
-const a = []
-const b = []
-let check = 0;
-First: for(let i = 0; i < 2 ; i++){
-    a[i] = prompt('Один из последних просмотренных фильмов?');
-    b[i] = prompt('На сколько оцените его? ');
-    if(a[i].length == 0 || a[i].length >= 50 || a[i] == '' || b[i] == '' || b[i].length == 0 || b[i].length >= 50 ){
-        i--;
-    }else{
-        personalMovieDB.movies[a[i]] = b[i];
+function rememberMyFilm(){
+    const a = []
+    const b = []
+    let check = 0;
+    First: for(let i = 0; i < 2 ; i++){
+        a[i] = prompt('Один из последних просмотренных фильмов?');
+        b[i] = prompt('На сколько оцените его? ');
+        if(a[i].length == 0 || a[i].length >= 50 || a[i] == '' || b[i] == '' || b[i].length == 0 || b[i].length >= 50 ){
+            i--;
+        }else{
+            personalMovieDB.movies[a[i]] = b[i];
+        }
+    }
+    console.log(personalMovieDB.movies);
+}
+
+rememberMyFilm();
+
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        alert("Просмотрено довольно мало фильмов");
+    } else if(personalMovieDB.count >=10 && personalMovieDB.count <=30){
+        alert("Вы классический зритель");
+    } else if(personalMovieDB.count > 30){
+        alert("Вы киноман");
+    
+    } else alert("Произошла ошибка");
+    
+    function showMyDB(privat){
+        if(privat === false){
+            console.log(personalMovieDB);
+        }
     }
 }
-console.log(personalMovieDB.movies);
-if(personalMovieDB.count < 10){
-    alert("Просмотрено довольно мало фильмов");
-} else if(personalMovieDB.count >=10 && personalMovieDB.count <=30){
-    alert("Вы классический зритель");
-} else if(personalMovieDB.count > 30){
-    alert("Вы киноман");
 
-} else alert("Произошла ошибка");
-
-function showMyDB(privat){
-    if(privat === false){
-        console.log(personalMovieDB);
-    }
-}
+detectPersonalLevel();
 
 function writeYourGenres(){
     let genres = [];
